8a8b0f833d363510817bc37eac8f6e0dee1ca834
Mykola Riabchenko <mriabchenko11@gmail.com>
2020-12-13 16:27:31 +0200

    update User model

--- app/user/models.py
+++ app/user/models.py
@@ -1,8 +1,15 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 from db.db import Base
+
+user_to_user = Table(
+    'user_to_user', Base.metadata,
+    Column("follower_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("followed_id", Integer, ForeignKey("user.id"), primary_key=True)
+)
 
 
 class User(Base):
@@ -10,13 +17,26 @@ class User(Base):
     id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
     email = Column(String, unique=True)
     hashed_password = Column(String)
-    # first_name = Column(String(255))
-    # last_name = Column(String(255))
+    full_name = Column(String(255))
     date_registrations = Column(DateTime(), default=datetime.utcnow, index=True)
     last_login = Column(DateTime(), nullable=True)
-    is_active = Column(Boolean(), default=False)
-    is_superuser = Column(Boolean(), default=False)
-    is_staff = Column(Boolean(), default=False)
+    is_active = Column(Boolean, default=False)
+    is_superuser = Column(Boolean, default=False)
+    is_staff = Column(Boolean, default=False)
     avatar = Column(String, nullable=True)
 
+    post = relationship("Post", back_populates="user")
 
+    comment = relationship("Comment", back_populates="user")
+
+    liked = relationship("PostLike",
+                         foreign_keys='PostLike.user_id',
+                         backref='user', lazy='dynamic'
+                         )
+
+    following = relationship("User",
+                             secondary="user_to_user",
+                             primaryjoin="User.id==user_to_user.c.follower_id",
+                             secondaryjoin="User.id==user_to_user.c.followed_id",
+                             backref="followers"
+                             )
